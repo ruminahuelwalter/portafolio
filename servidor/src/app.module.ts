c0d4067597cc7105002eb1979967b98bc453f316
Walter <ruminahuelwalter@gmail.com>
2024-09-20 19:56:56 -0300

    conexion postgreSQL pgAdmin Nest Docker

--- servidor/src/app.module.ts
+++ servidor/src/app.module.ts
@@ -12,7 +12,7 @@ import { ConfigModule } from '@nestjs/config';
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
       database: process.env.DB_NAME,
-      username: process.env.DB_USERNAME,
+      username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       autoLoadEntities: true,
       synchronize: true,
